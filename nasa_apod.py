788bb4bcc78b504548847cf9ba13fe4373f67805
jonsies <43277433+jonsies@users.noreply.github.com>
2021-05-05 15:42:04 -0500

    nasa_apod.py help_text variable is now a function call to its own function in py file: incl/help_info.py option variable is now a function call to its own function in py file: incl/options_info.py moved majority of global variables into functions

--- nasa_apod.py
+++ nasa_apod.py
@@ -15,107 +15,49 @@ sys.dont_write_bytecode = True
 
 from incl.options import *
 from incl.logging import *
-from incl.fileIO import *
+from incl.file_io import *
 from incl.progress import *
 
+from incl.options_info import *
+from incl.help_info import *
 
-script_python = os.path.basename(
-        "".join(sys.executable.split("/")[-1:]))
-script_name = sys.argv[0]
-script_version = ".055"
-script_url = "https://github.com/jonsies/nasa_apod.py"
-
-http_default_blocksize = 8192
-
-apod_floor_date = "19950620"
-apod_page_ext = "html"
-
-apod_regex_pattern = "href=\"image.+?\..{3}"
-apod_regex_lstrip = "href=\""
-apod_regex_rstrip = ""
-
-opt_name_apod_last_file = "apod_last_file"
-
-opt_conf_file_default = "config"
-opt_image_name_default = "apod"
-opt_response_file_default = "response.txt"
-opt_url_default = "https://apod.nasa.gov/apod/"
-
-opt_ignored_default_options = ["conf_file",
-        "conf_write_defaults","version","help"]
 
 logger = None
+script_version = ".056"
 
 
-help_text = ["Usage: {} {} [OPTION]...".format(script_python,script_name),
-        "",
-        "Mandatory arguments to long options are mandatory for short options too.",
-        "",
-        "Options:",
-        "  --conf-file=\"FILE\"           configuration file to read on application",
-        "                                 start to set options",
-        "                                 default: \"{}\"".format(opt_conf_file_default),
-        "                               passed parameters take precedence over",
-        "                                 configuration values",
-        "  --conf-write-defaults        write default option values to the",
-        "                                 configuration file and exit",
-        "  -c, --set-bg-cmd=\"CMD\"       command to use to set wallpaper for",
-        "                                 *nix based systems",
-        "  -d, --date=########          retrieve APOD for specific date",
-        "                                 format is YYYYMMDD",
-        "  -f, --force-update           disregard 24h file timestamp check and",
-        "                                 force APOD download",
-        "  -i, --image-name=\"NAME\"      name to use to save APOD image",
-        "                                 extension not required",
-        "                                 default: \"{}\"".format(opt_image_name_default),
-        "  -n, --no-update              do not request APOD from url, instead use",
-        "                                 prior APOD image file to set as",
-        "                                 wallpaper",
-        "  -r, --random                 request APOD of a random past date",
-        "                                 ignored if --date option is used",
-        "  -R, --response-file=\"FILE\"   set response file name for saving response",
-        "                                 file",
-        "                                 default: \"{}\"".format(opt_response_file_default),
-        "                                 see --save-response option",
-        "  -s, --save-only              request and save APOD but do not set",
-        "                                 wallpaper",
-        "  -S, --save-response          flag to enable saving of request response",
-        "                                 to response file",
-        "                                 see --response-file option",
-        "  -u, --url                    URL of APOD site",
-        "                                 default: \"{}\"".format(opt_url_default),
-        "  -v, --verbose                enable verbose mode",
-        "  -h, --help                   display this help and exit",
-        "  --version                    output version information and exit",
-        "",
-        "  Example: {} {} -v -i\"wallpaper\" --date=20210429".format(script_python,script_name),
-        "",
-        "{}".format(script_url)
-        ]
+def getRandomDate(floor_date,ceil_date=""):
+    if ceil_date == "":
+        date_ceil_date = datetime.now()
+    else:
+        date_ceil_date = datetime.strptime(
+                ceil_date,"%Y%m%d")
 
+    date_floor_date = datetime.strptime(
+            floor_date,"%Y%m%d")
 
-def getRandomDate():
-    floor_date = datetime.strptime(
-            apod_floor_date,"%Y%m%d")
-
-    ceil_date = datetime.now()
-
-    random_date = floor_date + (ceil_date - floor_date) * \
+    random_date = date_floor_date + \
+            (date_ceil_date - date_floor_date) * \
             random.random()
 
     return random_date
 
 
-def checkDate(date_string):
+def checkDate(date_string,floor_date,ceil_date=""):
     retval = ""
 
-    floor_date = datetime.strptime(
-            apod_floor_date,"%Y%m%d")
-    ceil_date = datetime.now()
+    if ceil_date == "":
+        date_ceil_date = datetime.now()
+    else:
+        date_ceil_date = datetime.strptime(
+                ceil_date,"%Y%m%d")
+
+    date_floor_date = datetime.strptime(
+            floor_date,"%Y%m%d")
 
     try:
         date = datetime.strptime(date_string,"%Y%m%d")
-        if date < floor_date or date > ceil_date:
+        if date < date_floor_date or date > date_ceil_date:
             retval = ""
         else:
             retval = date.strftime("%y%m%d")
@@ -139,7 +81,7 @@ def humanReadableSize(bytesize):
     return retval
 
 
-def urlRequest(url):
+def urlRequest(url,default_blocksize=8192):
     con_length = None
     con_charset = None
 
@@ -169,9 +111,9 @@ def urlRequest(url):
 
     if con_length:
         con_length = int(con_length)
-        blocksize = max(http_default_blocksize, con_length//100)
+        blocksize = max(default_blocksize, con_length//100)
     else:
-        blocksize = http_default_blocksize;
+        blocksize = default_blocksize;
 
     logger.logMessage("REQUEST CONTENT-TYPE: {}".format(con_type))
 
@@ -195,11 +137,11 @@ def urlRequest(url):
     buffer = io.BytesIO()
 
     if logger.verbose == True:
-        print_prefix = "{}: [MSG] - Downloading content: ".format(
-                script_name)
+        print_prefix = "{}: [MSG] - Downloading content ".format(
+                sys.argv[0])
     else:
-        print_prefix = "{}: Downloading content: ".format(
-                script_name)
+        print_prefix = "{}: Downloading content ".format(
+                sys.argv[0])
 
     while True:
         readbuf = response.read(blocksize)
@@ -265,64 +207,32 @@ def setWallpaper(image_file,set_bg_cmd):
 def main():
     global logger
 
-    source = ""
+    source = None
 
-    options = [
-            ["conf_file",
-                "","conf-file",True,
-                opt_conf_file_default,pyOptions.TYPE_STRING],
-            ["conf_write_defaults",
-                "","conf-write-defaults",False,
-                False,pyOptions.TYPE_BOOL],
-            ["set_bg_cmd",
-                "c","set-bg-cmd",True,
-                "/usr/bin/feh --bg-fill",pyOptions.TYPE_STRING],
-            ["date",
-                "d","date",True,
-                "",pyOptions.TYPE_STRING],
-            ["force_update",
-                "f","force-update",False,
-                False,pyOptions.TYPE_BOOL],
-            ["image_name",
-                "i","image-name",True,
-                opt_image_name_default,pyOptions.TYPE_STRING],
-            ["no_update",
-                "n","no-update",False,
-                False,pyOptions.TYPE_BOOL],
-            ["random",
-                "r","random",False,
-                False,pyOptions.TYPE_BOOL],
-            ["response_file",
-                "R","respone-file",True,
-                opt_response_file_default,pyOptions.TYPE_STRING],
-            ["save_only",
-                "s","save-only",False,
-                False,pyOptions.TYPE_BOOL],
-            ["save_response",
-                "S","save-response",False,
-                False,pyOptions.TYPE_BOOL],
-            ["url",
-                "u","url",True,
-                opt_url_default,pyOptions.TYPE_STRING],
-            ["verbose",
-                "v","verbose",False,
-                False,pyOptions.TYPE_BOOL],
-            [opt_name_apod_last_file,
-                "","",False,
-                "",pyOptions.TYPE_STRING],
-            ["version",
-                "","version",False,
-                False,pyOptions.TYPE_BOOL],
-            ["help",
-                "h","help",False,
-                False,pyOptions.TYPE_BOOL]
-        ]
+    apod_floor_date = "19950620"
+    apod_page_ext = "html"
+    apod_regex_pattern = "href=\"image.+?\..{3}"
+    apod_regex_lstrip = "href=\""
+    apod_regex_rstrip = ""
 
+    opt_ignored_default_options = ["conf_file",
+            "conf_write_defaults","version","help"]
 
-    logger = pyLogging(script_name)
+    logger = pyLogging(sys.argv[0])
 
-    s_opts = pyOptions(script_name,script_version,
+    options = getOptions()
+    s_opts = pyOptions(sys.argv[0],script_version,
             options,"conf_file")
+
+    help_text = getHelpText(sys.argv[0],script_version,
+            [
+                ["conf_file",s_opts.getDefaultOption("conf_file")],
+                ["image_name",s_opts.getDefaultOption("image_name")],
+                ["response_file",s_opts.getDefaultOption(
+                    "response_file")],
+                ["url",s_opts.getDefaultOption("url")],
+            ])
+
     s_opts.setLogger(logger)
     s_opts.setHelpText(help_text)
     s_opts.readOptions(sys.argv)
@@ -335,7 +245,6 @@ def main():
         logger.printMessage("Exiting.")
         sys.exit(0)
 
-
     opt_url = s_opts.getOption("url")
     opt_no_update = s_opts.getOption("no_update")
     opt_force_update = s_opts.getOption("force_update")
@@ -346,7 +255,7 @@ def main():
     opt_save_response = s_opts.getOption("save_response")
     opt_save_only = s_opts.getOption("save_only")
     opt_setbg_cmd = s_opts.getOption("set_bg_cmd")
-    opt_apod_last_file = s_opts.getOption(opt_name_apod_last_file)
+    opt_apod_last_file = s_opts.getOption("apod_last_file")
 
     options = None
 
@@ -370,7 +279,7 @@ def main():
     if opt_date != "":
         logger.logMessage("EXACT DATE SPECIFIED: {}".format(
             opt_date))
-        date_string = checkDate(opt_date)
+        date_string = checkDate(opt_date,apod_floor_date)
         if date_string == "":
             logger.showError(
                     "{}: date option value invalid ({})".format(
@@ -381,7 +290,7 @@ def main():
         date_string = None
 
     elif opt_random == True:
-        date = getRandomDate()
+        date = getRandomDate(apod_floor_date)
         opt_date = datetime.strftime(date,"%Y%m%d")
         logger.logMessage("RANDOM DATE GENERATED: {}".format(
             opt_date))
@@ -425,6 +334,9 @@ def main():
                 fileWrite(image_file_name,source,binary=True,
                         logger=logger)
 
+                logger.printMessage("Image saved: {}".format(
+                    image_file_name))
+
             else:
                 logger.showError("{}: {}".format(
                     "Error Occurred","Could not retrieve image URL"))
@@ -443,8 +355,8 @@ def main():
 
         if image_file_name != "" and fileExists(image_file_name):
             setWallpaper(image_file_name,opt_setbg_cmd)
-            s_opts.setOption(opt_name_apod_last_file,image_file_name)
-            s_opts.writeOptions([opt_name_apod_last_file],
+            s_opts.setOption("apod_last_file",image_file_name)
+            s_opts.writeOptions(["apod_last_file"],
                     opt_ignored_default_options)
 
         else:
